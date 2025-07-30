a5a562b92d84b96962c44c3cbde4d4687e9d0b62
Panevnyk <v.panevnyk@gmail.com>
2025-07-30 11:21:00 +0300

    update tag_release script

--- RetouchMain/ci_scripts/ci_post_xcodebuild.sh
+++ RetouchMain/ci_scripts/ci_post_xcodebuild.sh
@@ -4,9 +4,9 @@ set -e
 echo "Running ci_post_xcodebuild.sh"
 echo "Branch: $CI_BRANCH"
 
-if [[ "$CI_BRANCH" == release/* ]]; then
+if [[ "$CI_BRANCH" == release/* && "$CI_XCODEBUILD_ACTION" == "archive" ]]; then
   echo "Running tag_release.sh for branch: $CI_BRANCH"
   sh "$CI_PRIMARY_REPOSITORY_PATH/RetouchMain/ci_scripts/tag_release.sh"
 else
-  echo "Not a release branch. Skipping tag."
+  echo "Skipping tag. Not a release branch or not archive step."
 fi
--- RetouchMain/ci_scripts/tag_release.sh
+++ RetouchMain/ci_scripts/tag_release.sh
@@ -4,9 +4,11 @@ set -e
 
 echo "Running ci_post_xcodebuild.sh: Auto-tagging"
 
-VERSION=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" "$CI_PRIMARY_REPOSITORY_PATH/RetouchMain/RetouchMain/Info.plist")
-BUILD=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "$CI_PRIMARY_REPOSITORY_PATH/RetouchMain/RetouchMain/Info.plist")
+#VERSION=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" "$CI_PRIMARY_REPOSITORY_PATH/RetouchMain/RetouchMain/Info.plist")
+#BUILD=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "$CI_PRIMARY_REPOSITORY_PATH/RetouchMain/RetouchMain/Info.plist")
 
+VERSION=$(xcodebuild -showBuildSettings -scheme RetouchMain | awk -F " = " '/MARKETING_VERSION/ {print $2}')
+BUILD=$(xcodebuild -showBuildSettings -scheme RetouchMain | awk -F " = " '/CURRENT_PROJECT_VERSION/ {print $2}')
 TAG="v$VERSION"
 
 echo "Tag to create: $TAG"
