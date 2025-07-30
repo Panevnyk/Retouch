814e445a4fbb91d6b925e76547820e40f542b86a
Panevnyk <v.panevnyk@gmail.com>
2025-07-30 11:44:38 +0300

    update tag_release script

--- RetouchMain/ci_scripts/ci_post_xcodebuild.sh
+++ RetouchMain/ci_scripts/ci_post_xcodebuild.sh
@@ -5,8 +5,11 @@ echo "Running ci_post_xcodebuild.sh"
 echo "Branch: $CI_BRANCH"
 
 if [[ "$CI_BRANCH" == release/* && "$CI_XCODEBUILD_ACTION" == "archive" ]]; then
-  echo "Running tag_release.sh for branch: $CI_BRANCH"
-  sh "$CI_PRIMARY_REPOSITORY_PATH/RetouchMain/ci_scripts/tag_release.sh"
+    if [[ -n $CI_APP_STORE_SIGNED_APP_PATH ]]; # checks if there is an AppStore signed archive after running xcodebuild
+    then
+        echo "Running tag_release.sh for branch: $CI_BRANCH"
+        sh "$CI_PRIMARY_REPOSITORY_PATH/RetouchMain/ci_scripts/tag_release.sh"
+    fi
 else
-  echo "Skipping tag. Not a release branch or not archive step."
+    echo "Skipping tag. Not a release branch or not archive step."
 fi
